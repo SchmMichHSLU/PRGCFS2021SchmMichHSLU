d9486479af45de899f5f28f41ba42e7a83ccbac7
SchmMichHSLU <michael.schmid.01@stud.hslu.ch>
2021-03-25 20:32:37 +0100

    SW05

---
+++ 21_Workshops/06_functions/recursion.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// function declaration
+long factorial(int n);
+long nchoosek(int n, int k);
+char todo;
+int end = 0, n = 0, k = 0;
+long result = 0;
+
+int main (int argc, char *argv[])
+{
+	
+	if (argc == 3)
+	{
+		todo = 'v';
+		n = atoi(argv[1]);
+		k = atoi(argv[2]);
+	}else if(argc == 2)
+	{
+		todo = 'l';
+		end = atoi(argv[1]);
+	}else
+	{
+		printf("Wrong input! 2 values for specific binomial coefficient, 1 for declaring ending of loop");
+	}
+	
+	switch(todo)
+	{
+	case('v'): 
+		printf("Der Binominalkoeffizient von %d und %d ist %ld\n", n, k, nchoosek(n,k));
+		break;
+		
+	case('l'):
+		for(int n = 0; n<=end; n++)
+		{			
+			for(int k = 0; k<=end; k++)
+			{
+				printf("Der Binominalkoeffizient von %d und %d ist %ld\n", n, k, nchoosek(n,k));
+			}
+		}
+		break;
+	}
+
+	return 0;
+}
+
+//function implementation (factorial)
+long factorial(int n)
+{
+	result = (n>1) ? (n*factorial(n-1)) : 1;
+	return result;
+}
+
+//function implementation (nchoosek)
+long nchoosek(int n, int k)
+{
+	result = factorial(n) / (factorial(k)*(factorial(n-k)));
+	return result;
+}
